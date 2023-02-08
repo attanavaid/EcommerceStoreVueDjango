735e240989d2b7cd4c2fb2bb974469c8ddf13b8a
Atta Navaid <attanavaid@gmail.com>
2023-02-07 19:14:43 -0500

    Updated CORS

--- attasstore/attasstore/settings.py
+++ attasstore/attasstore/settings.py
@@ -54,6 +54,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://localhost:8080',
     'http://127.0.0.1:8000',
+    'https://ecommerce-store-vue-django.vercel.app/'
 ]
 
 MIDDLEWARE = [
